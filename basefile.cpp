2361f9d57321f0e904739ec7ee96293cca9b3a2a
eazyreal <0312fs3@gmail.com>
2019-12-26 02:07:39 +0800

    cf interactive 610D

--- basefile.cpp
+++ basefile.cpp
@@ -1,16 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef longlong LL;
-//for (auto& x : xs)
-
+typedef long long LL;
 int main()
 {
   std::ios::sync_with_stdio(false);
   //std::cin.tie(0);
-  fstream fin("filename.in");
-
-
+  //fstream fin("filename.in");
 
   return 0;
 }
