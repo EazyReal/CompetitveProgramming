67984d8eaea3f057a818b4eb9ceca41ddd965fc3
EazyReal <0312fs3@gmail.com>
2020-02-10 21:53:46 +0800

    618

--- 20200209cf618/D.cpp
+++ 20200209cf618/D.cpp
@@ -18,10 +18,28 @@ typedef long double ld;
 
 int T, n;
 
+pii add(pii a, pii b)
+{
+  return mp(a.X+b.X, a.Y+b.Y);
+}
+
 signed main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     //freopen("in", "r", stdin);
+    cin >> n;
+    vector<pii> a(n);
+    for(auto &ai : a) cin >> ai.X >> ai.Y;
+    if(n%2 == 1) cout << "NO" << endl;
+    else //even n and point-symmetric => YES, else => NO
+    {
+      pii axis;
+      axis = add(a[0], a[n/2]); //ignore/2
+      bool flag = 1;
+      rep(i, 1, n/2) flag &= (add(a[i], a[i+n/2]) == axis);
+      if(flag) cout << "YES" << endl;
+      else cout << "NO" << endl;
+    }
     return 0;
 }
