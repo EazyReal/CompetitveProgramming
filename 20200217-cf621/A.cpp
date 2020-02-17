9cd2556f3cc9ec1488aa36a1083bbdada18b3550
yt lin <0312fs3@gmail.com>
2020-02-18 00:16:49 +0800

    mac

---
+++ 20200217-cf621/A.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define X first
+#define Y second
+#define pb push_back
+#define mp make_pair
+#define all(a) a.begin(), a.end()
+#define rep(i, st, n) for (int i = (st); i < (n); ++i)
+#define repinv(i, st, n) for (int i = ((n)-1); i >= (st); --i)
+#define MEM(a, b) memset(a, b, sizeof(a));
+
+#define debug(x) std::cout << #x << ": " << x << endl
+#define fastIO() ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define fileIO(in, out) freopen(in, "r", stdin); freopen(out, "w", stdout)
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef long double ld;
+
+//mt19937 mrand(random_device{}());
+const ll mod=1e9+7;
+//int rnd(int x) { return mrand() % x;}
+ll powmod(ll a,ll b) {ll res=1;a%=mod; assert(b>=0); for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
+ll gcd(ll a,ll b) { return b?gcd(b,a%b):a;}
+//INT_MAX, ULLONG_MAX, LLONG_MAX or numeric_limits<int>::min()
+
+//------------------------------------------------------------------------//
+int T;
+const int maxn = mod;
+int n;
+int d;
+
+void solve()
+{
+  cin >> n >> d;
+  vector<int> a(n);
+  for(auto&ai : a) cin >> ai;
+  int ans = a[0];
+  rep(i, 1, n)
+  {
+    if(d < i) break;
+    int canmove = min(d/i, a[i]);
+    ans += canmove;
+    d -= canmove*i;
+  }
+  cout << ans << endl;
+  return;
+}
+
+
+signed main()
+{
+  fastIO();
+  cin >> T;
+  //T = 1;
+  while(T--) solve();
+  return 0;
+}
