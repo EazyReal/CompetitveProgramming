618cae8078f72e7d1482e0a5c0351e3905641f7d
yt lin <0312fs3@gmail.com>
2020-03-21 13:43:29 +0800

    cf

---
+++ Archieves/kickstart/kickstart2019A/C.cpp
@@ -0,0 +1,22 @@
+/*[ ]
+ [    ]
+     [           ]
+       [    ]
+       
+no overlap => id
+inclusion => inner first
+sort by len ? 
+[-----]
+   [-----]
+  [---------]
+  if same len + inclusion => 0 can assert no
+
+reverse to ignore order of previous : hint 1
+
+blabla [  ] union of other can be calc ?
+[ .... ] [.... ] 
+
+
+shorter first is better + maybe segment operation (my)
+ans longer last can be proven and + segment tree*/
+
