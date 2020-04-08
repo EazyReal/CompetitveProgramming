231b19fc9cf902dd43480721030a2bada957f265
eazyreal <0312fs3@gmail.com>
2020-04-09 01:07:20 +0800

    cf

---
+++ 20200408-cf-632/B.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+#define LOCAL
+using namespace std;
+
+#define X first
+#define Y second
+#define pb push_back
+#define mp make_pair
+#define all(a) a.begin(), a.end()
+#define rep(i, st, n) for (int i = (st); i < (n); ++i)
+#define repinv(i, st, n) for (int i = ((n)-1); i >= (st); --i)
+#define MEM(a, b) memset(a, b, sizeof(a));
+#ifdef LOCAL
+#define debug(x) std::cerr << #x << ": " << x << endl
+#else
+#define debug(x) 860111
+#endif
+#define fastIO() ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define fileIO(in, out) freopen(in, "r", stdin); freopen(out, "w", stdout)
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef long double ld;
+
+//mt19937 mrand(random_device{}());
+//mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+const ll mod=1e9+7;
+//int rnd(int x) { return mrand() % x;}
+ll powmod(ll a,ll b) {ll res=1;a%=mod; assert(b>=0); for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
+ll gcd(ll a,ll b) { return b?gcd(b,a%b):a;}
+pii operator+(const pii&x, const pii&y) { return mp(x.X+y.X, x.Y+y.Y);}
+pii operator-(const pii&x, const pii&y) { return mp(x.X-y.X, x.Y-y.Y);}
+//INT_MAX, ULLONG_MAX, LLONG_MAX or numeric_limits<int>::min()
+
+inline ll read(){
+	char ch=getchar();ll x=0,f=0;
+	while(ch<'0' || ch>'9') f|=ch=='-',ch=getchar();
+	while(ch>='0' && ch<='9') x=x*10+ch-'0',ch=getchar();
+	return f?-x:x;
+}
+
+//------------------------------------------------------------------------//
+int T;
+const int maxn = 2e5+7;
+int n;
+ll a[maxn];
+ll b[maxn];
+ll dp[maxn];
+ll dm[maxn];
+//check T
+void solve()
+{
+	cin >> n;
+  rep(i, 0, n) cin >> a[i];
+  rep(i, 0, n) cin >> b[i];
+  ll INF = 1e9+1;
+  //ll dp = INF, dm = INF; 
+  dp[0] = dm[0] = INF;
+  rep(i, 0, n-1)
+  {
+    if(a[i] == 0) dm[i+1] = dm[i], dp[i+1] = dp[i];
+    if(a[i] < 0ll) dm[i+1] = min(dm[i], -a[i]), dp[i+1] = dp[i];
+    if(a[i] > 0ll) dp[i+1] = min(dp[i], a[i]), dm[i+1] = dm[i];
+  }
+  /*rep(i, 0, n)
+  {
+    //cout << dm[i] << " " << dp[i] << endl;
+  }*/
+  bool flag = 1;
+  repinv(i, 0, n)
+  {
+    //debug(i); debug(dp[i]); debug(dm[i]);
+    if(a[i] < b[i])
+    {
+      if(abs(a[i]-b[i])%dp[i] != 0) flag = 0; 
+    }else if(a[i] > b[i]){
+      if(abs(a[i]-b[i])%dm[i] != 0) flag = 0;
+    }
+    //if(flag == 0) debug(i);
+  }
+  cout << (flag?"YES":"NO") << endl;
+  return;
+}
+
+
+signed main()
+{
+  fastIO();
+  T = 1;
+  cin >> T; //this
+  rep(tc, 1, T+1)
+  {
+    //cout << "Case #" << tc << ": ";
+    solve();
+  }
+  return 0;
+}
